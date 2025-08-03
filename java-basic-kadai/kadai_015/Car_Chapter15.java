f3c48b810f01d18862a798d635b2ce3a473c1df1
shoichi22 <shoichi.de.l@gmail.com>
2025-08-03 23:25:28 +0900

    kadai_015をアップロード

---
+++ java-basic-kadai/kadai_015/Car_Chapter15.java
@@ -0,0 +1,22 @@
+package kadai_015;
+
+public class Car_Chapter15 {
+
+	private int gear = 1;
+	private int speed = 10;
+	
+	public void changeGear(final int afterGear) {
+		switch (afterGear) {
+			case 1 -> speed=10;
+			case 2 -> speed=20;
+			case 3 -> speed=30;
+			case 4 -> speed=40;
+			default -> speed=10;
+		}
+		System.out.println("ギア"+gear+"から"+afterGear+"に切り替えました");
+	}
+	
+	public void run() {
+		System.out.println("速度は時速"+speed+"です");
+	}
+}
