8fd0ba51aca8cb7a66583944aeb048a2898d5d11
shoichi22 <shoichi.de.l@gmail.com>
2025-08-05 23:47:06 +0900

    kadai_021のアップロード(2回目)

---
+++ java-basic-kadai/kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,13 @@
+package kadai_021;
+
+public class DictionaryExec_Chapter21 {
+
+	public static void main(String[] args) {
+		Dictionary_Chapter21 dict = new Dictionary_Chapter21();
+		
+		String[] desires = {"apple","banana","grape","orange"};
+		dict.dictSearch(desires);
+
+	}
+
+}
