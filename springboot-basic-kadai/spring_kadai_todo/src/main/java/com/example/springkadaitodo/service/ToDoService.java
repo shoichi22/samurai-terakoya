5fa4a5a2ce1de86e265084d2b31450d79ab15026
shoichi22 <shoichi.de.l@gmail.com>
2025-08-22 11:09:48 +0900

    spring_kadai_todoのアップロード(2回目)

---
+++ springboot-basic-kadai/spring_kadai_todo/src/main/java/com/example/springkadaitodo/service/ToDoService.java
@@ -0,0 +1,45 @@
+package com.example.springkadaitodo.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.springkadaitodo.entity.ToDo;
+import com.example.springkadaitodo.repository.ToDoRepository;
+
+@Service
+public class ToDoService {
+	private final ToDoRepository todoRepository;
+
+    // 依存性の注入（DI）を行う（コンストラクタインジェクション）
+    public ToDoService(ToDoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    // 新規ユーザーの登録メソッド
+    public void createToDo(String title, String priority, String status) {
+        // ユーザー名の未入力チェック（空欄はNG）
+        if (title == null || title.isEmpty()) {
+            throw new IllegalArgumentException("タイトルを入力してください。");
+        }
+
+        // ユーザー名の重複チェック（完全一致はNG）
+        if (!todoRepository.findByTitle(title).isEmpty()) {
+            throw new IllegalArgumentException("そのタイトルは既に使用されています。");
+        }
+
+        // ユーザー登録用のエンティティを作成
+        ToDo todo = new ToDo();
+        todo.setTitle(title);
+        todo.setPriority(priority);
+        todo.setStatus(status);
+
+        // ユーザーの登録
+        todoRepository.save(todo);
+    }
+
+    // ユーザーの一括取得メソッド
+    public List<ToDo> getAllToDos() {
+        return todoRepository.findAll();
+    }
+}
